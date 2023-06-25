39d81f4c11bc65ecbcfb50ceaf8cbd4827afb59c
melikesseda <119448244+melikesseda@users.noreply.github.com>
2023-06-25 20:54:06 +0300

    Update and rename workbench_sqlFiles.zip to doctor.sql

---
+++ doctor.sql
@@ -0,0 +1,24 @@
+SELECT * FROM hospital.doctor;
+
+INSERT INTO doctor (id, name, surname, gender, branch, graduation_date, graduation_university, salary)
+VALUES
+(1, 'Anthony', 'Brown', 'Male', 'Cardiology', '2001-02-03', 'Harvard University', 20000.00),
+(2, 'Andrew', 'Khan', 'Male', 'Obstetrician', '1995-05-15', 'Harvard University', 60000.00),
+(3, 'Amelia', 'Write', 'Female', 'Neurology', '1994-08-25', 'Oxford University', 70000.00),
+(4, 'Anthony', 'Robins', 'Male', 'Cardiology', '1989-01-10', 'University of Sydney', 32000.00),
+(5, 'Sophia', 'Mitchell', 'Female', 'Nephrology', '1977-02-12', 'University of Melbourne', 46000.00),
+(6, 'Benjamin', 'Anderson', 'Male', 'Eye Diseases', '1975-08-18', 'University of Toronto', 89000.00),
+(7, 'Isabella', 'Wilson', 'Female', 'Rheumatology', '1985-07-14', 'Karolinska Institute', 39000.00),
+(8, 'Noah', 'Thompson', 'Male', 'Orthopedics', '1999-08-11', 'University of British Columbia', 45000.00),
+(9, 'Olivia', 'Davis', 'Female', 'Obstetrician', '1986-09-04', 'Harvard University', 68000.00),
+(10, 'Ethan', 'Johnson', 'Male', 'Rheumatology', '2021-07-10', 'Stanford University', 49000.00),
+(11, 'Ava', 'Taylor', 'Female', 'Eye Diseases', '2018-05-20', 'Cambridge University', 39000.00),
+(12, 'Alexander', 'Martinez', 'Male', 'Nephrology', '2003-03-03', 'Harvard University', 61000.00),
+(13, 'Mehmet', 'Öz', 'Male', 'Pulmonology', '1996-01-03', 'Hacettepe University', 72000.00),
+(14, 'Canan', 'Karatoy', 'Female', 'Endocrinology', '1975-09-16', 'Koç University', 55000.00),
+(15, 'Alexander', 'Rose', 'Male', 'Psychiatry', '1980-10-30', 'Oxford University', 50000.00),
+(16, 'Alp', 'Çelik', 'Male', 'Psychiatry', '2005-07-16', 'İnönü University', 35000.00);
+
+
+
+
