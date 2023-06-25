1d5690af7644e06c08e6bb271ea3d3c4bc41558a
melikesseda <119448244+melikesseda@users.noreply.github.com>
2023-06-25 20:55:19 +0300

    Create medicine.sql

---
+++ medicine.sql
@@ -0,0 +1,5 @@
+SELECT * FROM hospital.medicine;
+
+INSERT INTO medicine (MedicationName, PrescriptionNo, ProductionDate, ExpiryDate, StockStatus)
+VALUES ('PARANOX', 'M70531', '2020-11-03', '2023-01-01', 0);
+
